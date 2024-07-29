7a3d107f29511c585116c24f45a897f59e159cf2
bouygun <berceozumm@gmail.com>
2024-07-29 09:16:26 +0300

    feat: linkedlist vs arraylist examples

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //* ArrayLists
+//! when add smt the list get bigger
+//! search process -> O(1)
+//! remove process -> O(n)
+        //? create a list
+        ArrayList cities = new ArrayList();
+        cities.add("Ankara");
+        cities.add("İstanbul");
+        cities.add("Bursa");
+        cities.add("Ankara"); // can add the same value
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+        System.out.println(cities); // [Ankara, İstanbul, Bursa, Ankara]
+        cities.forEach(city -> System.out.println(city));
+
+
+        //? if has memory location
+        cities.add(2,"İzmir");
+        System.out.println(cities); // [Ankara, İstanbul, İzmir, Bursa, Ankara]
+
+        cities.remove(2);
+        System.out.println(cities); // [Ankara, İstanbul, Bursa, Ankara]
+
+        //? there is no memory location
+        int middleIndex = cities.size() / 2; // 4 / 2
+        System.out.println("middleIndex: "+ middleIndex);
+        cities.add(middleIndex, "X");
+        System.out.println("Add into middle: " + cities); // [Ankara, İstanbul, X, Bursa, Ankara]
+
+        middleIndex = cities.size() / 2;
+        System.out.println("middleIndex: "+ middleIndex);
+        cities.remove(middleIndex);
+        System.out.println("remove from middle " + cities); // [Ankara, İstanbul, Bursa, Ankara]
+
+        //* LinkedLists
+//! search process -> O(n)
+//! remove process -> O(1) because of doubly-linked
+        //? create a linked list
+        LinkedList linkedCities = new LinkedList();
+        linkedCities.add("Tokyo");
+        linkedCities.add("Paris");
+        linkedCities.add("Berlin");
+
+        System.out.println(linkedCities); // [Tokyo, Paris, Berlin]
+        linkedCities.forEach(city -> System.out.println(city));
+
+        //? if has memory location
+        linkedCities.add(1,"NewYork");
+        System.out.println(linkedCities); // [Tokyo, NewYork, Paris, Berlin]
+
+        linkedCities.remove(1);
+        System.out.println(linkedCities); // [Tokyo, Paris, Berlin]
+
+
+        //? there is no memory location
+        int linkedMiddleIndex = linkedCities.size() / 2; // 3 / 2
+        System.out.println("linkedMiddleIndex: "+ linkedMiddleIndex);
+        linkedCities.add(linkedMiddleIndex, "Y");
+        System.out.println("Add into middle: " + linkedCities); // [Tokyo, Y, Paris, Berlin]
+
+        linkedMiddleIndex = linkedCities.size() / 2;
+        System.out.println("linkedMiddleIndex: "+ linkedMiddleIndex);
+        linkedCities.remove(linkedMiddleIndex);
+        System.out.println("remove from middle " + linkedCities); // [Tokyo, Y, Berlin]
+
+    }}
